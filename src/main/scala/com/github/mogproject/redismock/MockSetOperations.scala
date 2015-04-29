b1be4417141d89255a7a24e91eeda76d6e867971
Yosuke Mizutani <mogproj@gmail.com>
2015-04-30 00:01:11 +0900

    refactor MockSetOperations

--- src/main/scala/com/github/mogproject/redismock/MockSetOperations.scala
+++ src/main/scala/com/github/mogproject/redismock/MockSetOperations.scala
@@ -2,53 +2,64 @@ package com.github.mogproject.redismock
 
 import com.github.mogproject.redismock.entity.{Bytes, SET, Key, SetValue}
 import com.github.mogproject.redismock.storage.Storage
+import com.github.mogproject.redismock.util.ops._
 import com.redis.{SetOperations, Redis}
 import com.redis.serialization._
 
 trait MockSetOperations extends SetOperations with MockOperations with Storage {
   self: Redis =>
 
-  private[this] def setRaw(key: Any, rawValue: SET.DataType)(implicit format: Format): Unit =
+  private def setRaw(key: Any, rawValue: SET.DataType)(implicit format: Format): Unit =
     currentDB.update(Key(key), SetValue(rawValue))
 
-  private[this] def getRaw(key: Any)(implicit format: Format): Option[SET.DataType] =
+  private def getRaw(key: Any)(implicit format: Format): Option[SET.DataType] =
     currentDB.get(Key(format.apply(key))).map(_.as(SET))
 
-  private[this] def getRawOrEmpty(key: Any)(implicit format: Format): SET.DataType =
+  private def getRawOrEmpty(key: Any)(implicit format: Format): SET.DataType =
     getRaw(key).getOrElse(Set.empty[Bytes])
 
+  private def reduceRaw(op: (Set[Bytes], Set[Bytes]) => Set[Bytes])
+                       (keys: Traversable[Any])
+                       (implicit format: Format): Set[Bytes] = keys.view.map(getRawOrEmpty).reduceLeft(op)
+
+  private def getReduced[A](op: (Set[Bytes], Set[Bytes]) => Set[Bytes])
+                           (key: Any, keys: Any*)
+                           (implicit format: Format, parse: Parse[A]): Option[Set[Option[A]]] = withDB {
+    (reduceRaw(op)(key :: keys.toList) map {_.parseOption(parse)}) |> Some.apply
+  }
+
+  private def storeReduced(op: (Set[Bytes], Set[Bytes]) => Set[Bytes])
+                          (key: Any, keys: Any*)
+                          (implicit format: Format): Option[Long] = withDB {
+    reduceRaw(op)(keys) <| {setRaw(key, _)} |> { xs => Some(xs.size)}
+  }
 
   // SADD (VARIADIC: >= 2.4)
   // Add the specified members to the set value stored at key.
-  override def sadd(key: Any, value: Any, values: Any*)(implicit format: Format): Option[Long] = {
-    currentDB.synchronized {
-      val a = getRawOrEmpty(key)
-      val b = a ++ (value :: values.toList).map(Bytes.apply)
-      setRaw(key, b)
-      Some(b.size - a.size)
-    }
+  override def sadd(key: Any, value: Any, values: Any*)(implicit format: Format): Option[Long] = withDB {
+    val a = getRawOrEmpty(key)
+    val b = a ++ (value :: values.toList).map(Bytes.apply)
+    setRaw(key, b)
+    Some(b.size - a.size)
   }
 
   // SREM (VARIADIC: >= 2.4)
   // Remove the specified members from the set value stored at key.
-  override def srem(key: Any, value: Any, values: Any*)(implicit format: Format): Option[Long] = {
-    currentDB.synchronized {
-      val a = getRawOrEmpty(key)
-      val b = a -- (value :: values.toList).map(Bytes.apply)
-      setRaw(key, b)
-      Some(a.size - b.size)
-    }
+  override def srem(key: Any, value: Any, values: Any*)(implicit format: Format): Option[Long] = withDB {
+    val a = getRawOrEmpty(key)
+    val b = a -- (value :: values.toList).map(Bytes.apply)
+    setRaw(key, b)
+    Some(a.size - b.size)
   }
 
   // SPOP
   // Remove and return (pop) a random element from the Set value at key.
-  override def spop[A](key: Any)(implicit format: Format, parse: Parse[A]): Option[A] = {
-    currentDB.synchronized {
-      val (h, t) = getRawOrEmpty(key).splitAt(1)
-      h.headOption.map { x =>
-        setRaw(key, t)
-        x.parse(parse)
-      }
+  override def spop[A](key: Any)(implicit format: Format, parse: Parse[A]): Option[A] = withDB {
+    // TODO: random choice
+    val (h, t) = getRawOrEmpty(key).splitAt(1)
+    h.headOption.map { x =>
+      setRaw(key, t)
+      x.parse(parse)
     }
   }
 
@@ -57,12 +68,8 @@ trait MockSetOperations extends SetOperations with MockOperations with Storage {
   // Integer reply, specifically:
   //   1 if the element is moved.
   //   0 if the element is not a member of source and no operation was performed.
-  override def smove(sourceKey: Any, destKey: Any, value: Any)(implicit format: Format): Option[Long] = {
-    currentDB.synchronized {
-      val ret = srem(sourceKey, value)
-      if (ret.exists(_ > 0)) sadd(destKey, value)
-      ret
-    }
+  override def smove(sourceKey: Any, destKey: Any, value: Any)(implicit format: Format): Option[Long] = withDB {
+    srem(sourceKey, value) <| { ret => if (ret.exists(_ > 0)) sadd(destKey, value)}
   }
 
   // SCARD
@@ -76,82 +83,63 @@ trait MockSetOperations extends SetOperations with MockOperations with Storage {
 
   // SINTER
   // Return the intersection between the Sets stored at key1, key2, ..., keyN.
-  override def sinter[A](key: Any, keys: Any*)(implicit format: Format, parse: Parse[A]): Option[Set[Option[A]]] = {
-    currentDB.synchronized {
-      Some((key :: keys.toList).map(getRawOrEmpty).reduceLeft(_ & _).map(_.parseOption(parse)))
-    }
-  }
+  override def sinter[A](key: Any, keys: Any*)(implicit format: Format, parse: Parse[A]): Option[Set[Option[A]]] =
+    getReduced(_ & _)(key, keys: _*)
 
   // SINTERSTORE
   // Compute the intersection between the Sets stored at key1, key2, ..., keyN,
   // and store the resulting Set at dstkey.
   // SINTERSTORE returns the size of the intersection, unlike what the documentation says
   // refer http://code.google.com/p/redis/issues/detail?id=121
-  override def sinterstore(key: Any, keys: Any*)(implicit format: Format): Option[Long] = {
-    currentDB.synchronized {
-      val xs = keys.map(getRawOrEmpty).reduceLeft(_ & _)
-      setRaw(key, xs)
-      Some(xs.size)
-    }
-  }
+  override def sinterstore(key: Any, keys: Any*)(implicit format: Format): Option[Long] =
+    storeReduced(_ & _)(key, keys: _*)
 
   // SUNION
   // Return the union between the Sets stored at key1, key2, ..., keyN.
-  override def sunion[A](key: Any, keys: Any*)(implicit format: Format, parse: Parse[A]): Option[Set[Option[A]]] = {
-    currentDB.synchronized {
-      Some((key :: keys.toList).map(getRawOrEmpty).reduceLeft(_ | _).map(_.parseOption(parse)))
-    }
-  }
+  override def sunion[A](key: Any, keys: Any*)(implicit format: Format, parse: Parse[A]): Option[Set[Option[A]]] =
+    getReduced(_ | _)(key, keys: _*)
 
   // SUNIONSTORE
   // Compute the union between the Sets stored at key1, key2, ..., keyN,
   // and store the resulting Set at dstkey.
   // SUNIONSTORE returns the size of the union, unlike what the documentation says
   // refer http://code.google.com/p/redis/issues/detail?id=121
-  override def sunionstore(key: Any, keys: Any*)(implicit format: Format): Option[Long] = {
-    currentDB.synchronized {
-      val xs = keys.map(getRawOrEmpty).reduceLeft(_ | _)
-      setRaw(key, xs)
-      Some(xs.size)
-    }
-  }
+  override def sunionstore(key: Any, keys: Any*)(implicit format: Format): Option[Long] =
+    storeReduced(_ | _)(key, keys: _*)
 
   // SDIFF
   // Return the difference between the Set stored at key1 and all the Sets key2, ..., keyN.
-  override def sdiff[A](key: Any, keys: Any*)(implicit format: Format, parse: Parse[A]): Option[Set[Option[A]]] = {
-    currentDB.synchronized {
-      Some((key :: keys.toList).map(getRawOrEmpty).reduceLeft(_ -- _).map(_.parseOption(parse)))
-    }
-  }
+  override def sdiff[A](key: Any, keys: Any*)(implicit format: Format, parse: Parse[A]): Option[Set[Option[A]]] =
+    getReduced(_ -- _)(key, keys: _*)
 
   // SDIFFSTORE
   // Compute the difference between the Set key1 and all the Sets key2, ..., keyN,
   // and store the resulting Set at dstkey.
-  override def sdiffstore(key: Any, keys: Any*)(implicit format: Format): Option[Long] = {
-    currentDB.synchronized {
-      val xs = keys.map(getRawOrEmpty).reduceLeft(_ -- _)
-      setRaw(key, xs)
-      Some(xs.size)
-    }
-  }
+  override def sdiffstore(key: Any, keys: Any*)(implicit format: Format): Option[Long] =
+    storeReduced(_ -- _)(key, keys: _*)
 
   // SMEMBERS
   // Return all the members of the Set value at key.
   override def smembers[A](key: Any)(implicit format: Format, parse: Parse[A]): Option[Set[Option[A]]] =
-    Some(getRawOrEmpty(key).map(_.parseOption(parse)))
+    getRawOrEmpty(key).map(_.parseOption(parse)) |> Some.apply
 
   // SRANDMEMBER
   // Return a random element from a Set
   override def srandmember[A](key: Any)(implicit format: Format, parse: Parse[A]): Option[A] =
+  // TODO: random choice
     getRawOrEmpty(key).headOption.map(_.parse(parse))
 
   // SRANDMEMBER
   // Return multiple random elements from a Set (since 2.6)
   override def srandmember[A](key: Any, count: Int)(implicit format: Format, parse: Parse[A]): Option[List[Option[A]]] =
-    Some(getRawOrEmpty(key).take(count).toList.map(_.parseOption(parse)))
+  // TODO: random choice
+    getRawOrEmpty(key).take(count).toList.map(_.parseOption(parse)) |> Some.apply
 
   // SSCAN
   // Incrementally iterate Set elements (since 2.8)
   override def sscan[A](key: Any, cursor: Int, pattern: Any = "*", count: Int = 10)(implicit format: Format, parse: Parse[A]): Option[(Option[Int], Option[List[Option[A]]])] =
-    send("SSCAN", key :: cursor :: ((x: List[Any]) => if (pattern == "*") x else "match" :: pattern :: x)(if (count == 10) Nil else List("count", count)))(asPair)
+  // TODO: implement
+    ???
+
+  //send("SSCAN", key :: cursor :: ((x: List[Any]) => if (pattern == "*") x else "match" :: pattern :: x)(if (count == 10) Nil else List("count", count)))(asPair)
 }
