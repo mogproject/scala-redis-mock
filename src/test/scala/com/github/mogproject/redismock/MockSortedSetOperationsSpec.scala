900a674eab7bf46c110a25ba472c9c005bb116ed
Yosuke Mizutani <mogproj@gmail.com>
2015-05-11 00:43:45 +0900

    add tests for MockSortedSetOperations closes #12

--- src/test/scala/com/github/mogproject/redismock/MockSortedSetOperationsSpec.scala
+++ src/test/scala/com/github/mogproject/redismock/MockSortedSetOperationsSpec.scala
@@ -27,14 +27,14 @@ with BeforeAndAfterAll {
 
   import r._
 
-  private def add = {
+  private def add() = {
     zadd("hackers", 1965, "yukihiro matsumoto") should equal(Some(1))
     zadd("hackers", 1953, "richard stallman", (1916, "claude shannon"), (1969, "linus torvalds"), (1940, "alan kay"), (1912, "alan turing")) should equal(Some(5))
   }
 
   describe("zadd") {
     it("should add based on proper sorted set semantics") {
-      add
+      add()
       zadd("hackers", 1912, "alan turing") should equal(Some(0))
       zcard("hackers").get should equal(6)
     }
@@ -42,7 +42,7 @@ with BeforeAndAfterAll {
 
   describe("zrem") {
     it("should remove") {
-      add
+      add()
       zrem("hackers", "alan turing") should equal(Some(1))
       zrem("hackers", "alan kay", "linus torvalds") should equal(Some(2))
       zrem("hackers", "alan kay", "linus torvalds") should equal(Some(0))
@@ -51,7 +51,7 @@ with BeforeAndAfterAll {
 
   describe("zrange") {
     it("should get the proper range") {
-      add
+      add()
       zrange("hackers").get should have size (6)
       zrangeWithScore("hackers").get should have size(6)
     }
@@ -59,7 +59,7 @@ with BeforeAndAfterAll {
 
   describe("zrank") {
     it ("should give proper rank") {
-      add
+      add()
       zrank("hackers", "yukihiro matsumoto") should equal(Some(4))
       zrank("hackers", "yukihiro matsumoto", reverse = true) should equal(Some(1))
     }
@@ -67,14 +67,14 @@ with BeforeAndAfterAll {
 
   describe("zremrangebyrank") {
     it ("should remove based on rank range") {
-      add
+      add()
       zremrangebyrank("hackers", 0, 2) should equal(Some(3))
     }
   }
 
   describe("zremrangebyscore") {
     it ("should remove based on score range") {
-      add
+      add()
       zremrangebyscore("hackers", 1912, 1940) should equal(Some(3))
       zremrangebyscore("hackers", 0, 3) should equal(Some(0))
     }
@@ -137,7 +137,7 @@ with BeforeAndAfterAll {
 
   describe("zcount") {
     it ("should return the number of elements between min and max") {
-      add
+      add()
 
       zcount("hackers", 1912, 1920) should equal(Some(2))
     }
@@ -145,7 +145,7 @@ with BeforeAndAfterAll {
 
   describe("zrangebyscore") {
     it ("should return the elements between min and max") {
-      add
+      add()
 
       zrangebyscore("hackers", 1940, true, 1969, true, None).get should equal(
         List("alan kay", "richard stallman", "yukihiro matsumoto", "linus torvalds"))
@@ -155,31 +155,31 @@ with BeforeAndAfterAll {
     }
 
     it("should return the elements between min and max and allow offset and limit") {
-      add
+      add()
 
-      zrangebyscore("hackers", 1940, true, 1969, true, Some(0, 2)).get should equal(
+      zrangebyscore("hackers", 1940, true, 1969, true, Some((0, 2))).get should equal(
         List("alan kay", "richard stallman"))
 
-      zrangebyscore("hackers", 1940, true, 1969, true, Some(0, 2), DESC).get should equal(
+      zrangebyscore("hackers", 1940, true, 1969, true, Some((0, 2)), DESC).get should equal(
         List("linus torvalds", "yukihiro matsumoto"))
 
-      zrangebyscore("hackers", 1940, true, 1969, true, Some(3, 1)).get should equal (
+      zrangebyscore("hackers", 1940, true, 1969, true, Some((3, 1))).get should equal (
         List("linus torvalds"))
 
-      zrangebyscore("hackers", 1940, true, 1969, true, Some(3, 1), DESC).get should equal (
+      zrangebyscore("hackers", 1940, true, 1969, true, Some((3, 1)), DESC).get should equal (
         List("alan kay"))
 
-      zrangebyscore("hackers", 1940, false, 1969, true, Some(0, 2)).get should equal (
+      zrangebyscore("hackers", 1940, false, 1969, true, Some((0, 2))).get should equal (
         List("richard stallman", "yukihiro matsumoto"))
 
-      zrangebyscore("hackers", 1940, true, 1969, false, Some(0, 2), DESC).get should equal (
+      zrangebyscore("hackers", 1940, true, 1969, false, Some((0, 2)), DESC).get should equal (
         List("yukihiro matsumoto", "richard stallman"))
     }
   }
 
   describe("zrangebyscoreWithScore") {
     it ("should return the elements between min and max") {
-      add
+      add()
 
       zrangebyscoreWithScore("hackers", 1940, true, 1969, true, None).get should equal(
         List(("alan kay", 1940.0), ("richard stallman", 1953.0), ("yukihiro matsumoto", 1965.0), ("linus torvalds", 1969.0)))
@@ -187,10 +187,10 @@ with BeforeAndAfterAll {
       zrangebyscoreWithScore("hackers", 1940, true, 1969, true, None, DESC).get should equal(
         List(("linus torvalds", 1969.0), ("yukihiro matsumoto", 1965.0), ("richard stallman", 1953.0),("alan kay", 1940.0)))
 
-      zrangebyscoreWithScore("hackers", 1940, true, 1969, true, Some(3, 1)).get should equal (
+      zrangebyscoreWithScore("hackers", 1940, true, 1969, true, Some((3, 1))).get should equal (
         List(("linus torvalds", 1969.0)))
 
-      zrangebyscoreWithScore("hackers", 1940, true, 1969, true, Some(3, 1), DESC).get should equal (
+      zrangebyscoreWithScore("hackers", 1940, true, 1969, true, Some((3, 1)), DESC).get should equal (
         List(("alan kay", 1940.0)))
     }
   }
@@ -200,10 +200,24 @@ with BeforeAndAfterAll {
   //
   describe("zincrby (additional") {
     it("should increase score") {
-      add
+      add()
 
       zincrby("hackers", 100.0, "alan kay") shouldBe Some(2040.0)
       zrangebyscoreWithScore("hackers", 2000, true, 2100, true, None).get shouldBe List(("alan kay", 2040.0))
+    }
+  }
+
+  describe("zscore (additional)") {
+    it("should return None if the key is missing") {
+      zscore("xyz", "") shouldBe None
+    }
+    it("should return None if the element is missing") {
+      add()
+      zscore("hackers", "") shouldBe None
+    }
+    it("should return score if the element exists") {
+      add()
+      zscore("hackers", "alan kay") shouldBe Some(1940.0)
     }
   }
 }
